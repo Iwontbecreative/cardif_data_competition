d25a4be2beb44988f1bbffe4b314ae4909f9c6ef
Thibault Févry <ThibaultFevry@gmail.com>
2016-02-14 13:24:48 +0100

    Added changes to use XGBoost

--- cardif.py
+++ cardif.py
@@ -1,13 +1,32 @@
 import pandas as pd
 from sklearn.cross_validation import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss
 from datetime import datetime
+import random
+random.seed(1)
+import xgboost as xgb
+
+# XGBoost here
+xgboost_params = {
+        "objective": "binary:logistic",
+        "booster": "gbtree",
+        "eval_metric": "logloss",
+        "eta": 0.01,
+        "subsample": 0.75,
+        "colsample_bytree": 0.68,
+        "max_depth": 7
+        }
+
 
 
 train = pd.read_csv('train.csv')
 labels = train.target
 train = train.drop(['target', 'ID', 'v22'], axis=1)
+# Feature importance > 0.01 with RF 100 est
+#train = train[['v10', 'v12', 'v21', 'v34', 'v40', 'v50', 
+    #'v114', 'v129']]
 
 # Transform text to categorical
 text_col = train.select_dtypes(['object']).columns
@@ -21,17 +40,36 @@ print(len(train.columns))
 
 # Fill na with most frequent value outside of NaNs
 for col in train.columns:
-    train[col].fillna(train[col].value_counts().index[0], inplace=True)
+    #train[col].fillna(train[col].value_counts().index[0], inplace=True)
+    train[col].fillna(0, inplace=True)
 
 Xtrain, Xtest, ytrain, ytest = train_test_split(train, labels, test_size=0.2)
 
 start = datetime.now()
 
-rfc = RandomForestClassifier(n_estimators=100)
-rfc.fit(Xtrain, ytrain)
+#rfc = RandomForestClassifier(n_estimators=100, n_jobs=-1)
+#rfc = LogisticRegression(penalty='l2', C=2)
+#rfc = KNeighborsClassifier(100)
+#rfc.fit(Xtrain, ytrain)
 
-ypred = rfc.predict_proba(Xtest)
-ypred = [i[1] for i in ypred]
+# XGBoost code
+xgtrain = xgb.DMatrix(Xtrain, ytrain)
+xgtest = xgb.DMatrix(Xtest)
+print('Fitting the model')
+boost_round = 1800
+clf = xgb.train(xgboost_params, xgtrain, num_boost_round=boost_round, evals=[(xgtrain, 'logloss')])
+
+print('Predicting')
+ypred = clf.predict(xgtest)
+
+
+
+#ypred = rfc.predict_proba(Xtest)
+#ypred = [i[1] for i in ypred]
+#for i, j in zip(train.columns, rfc.feature_importances_):
+#    if j > 0.005:
+#        print(i, j)
+
 print(ypred[:50])
 print(log_loss(ytest, ypred))
 print(datetime.now() - start)
